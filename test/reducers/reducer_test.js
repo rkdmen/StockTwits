fd7effa7563e768e02d44d335247f96b64dcfc33
John KM Jung <min85da@gmail.com>
2016-12-19 15:41:42 -0500

    additional test

--- test/reducers/reducer_test.js
+++ test/reducers/reducer_test.js
@@ -1,25 +1,7 @@
 import {renderComponent, expect} from '../test_helper';
 import {stockReducer} from '../../client/reducers/stockReducer';
-import {RETRIEVE_MSG, ADD_STOCK_JSON, GET_STOCK_JSON, DELETE_STOCK_JSON} from '../../client/constants/actionTypes';
-
-
-describe('stock Reducers', ()=> {
-
-  it('handles action with unknown type', ()=>{
-    expect(stockReducer(undefined, {})).to.eql({});
-  })
-
-  xit('handle action type RETRIEVE_MSG', ()=> {
-    const action = {
-      type: RETRIEVE_MSG,
-      payload:'AAPL'
-    };
-    expect(stockReducer({}, action)).to.eql([]);
-  });
-
-})
-
-
+import {userClickReducer} from '../../client/reducers/userClickReducer';
+import {RETRIEVE_MSG, ADD_STOCK_JSON, GET_STOCK_JSON, DELETE_STOCK_JSON, GET_USER_MESSAGE} from '../../client/constants/actionTypes';
 
 /*
 
@@ -32,3 +14,45 @@ Just ran into bunch of fail test, so I created basic testing for each component.
 I just need a small guidance for creating unit test and I should be good as senior test engineer guys.
 
 */
+
+describe('stockReducer', ()=> {
+
+  it('handles action with unknown type', ()=>{
+    expect(stockReducer(undefined, {})).to.eql({});
+  })
+
+  it('handle default action type RETRIEVE_MSG', ()=> {
+    const action = {
+      type: RETRIEVE_MSG,
+      payload: {}
+    };
+    expect(stockReducer({}, action.payload)).to.eql({});
+  });
+  it('should retrieve array of object when AAPL is requested', ()=> {
+    const action = {
+      type: RETRIEVE_MSG,
+      payload: {
+        messages:new Array(30)
+      }
+    };
+    expect(stockReducer({symbol:'AAPL'}, action.payload.messages)).to.be.instanceof(Object);
+  });
+})
+
+describe('userClickReducer ', ()=> {
+
+  it('handles userClickReducer as null when page load', ()=>{
+    expect(userClickReducer(null, {})).to.be.null;
+  })
+
+  it('handle default action type GET_USER_MESSAGE', ()=> {
+    const action = {
+      type: GET_USER_MESSAGE,
+      payload: {}
+    };
+    expect(stockReducer({}, action.payload)).to.eql({});
+  });
+
+
+})
+
